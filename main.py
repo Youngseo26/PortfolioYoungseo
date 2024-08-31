6f4e0546424d37ab570d373d48ef5d7de97b7689
김영서 <dudtj926@naver.com>
2024-08-31 20:31:36 +0900

    fix: python 백엔드01 main.py
    fix: python 백엔드01 main.py

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from fastapi.staticfiles import StaticFiles
 app = FastAPI()
 
 answer='TRAIN'
+
 @app.get('/answer')
 def get_answer():
-    return {'answer':answer}
+    return answer
 
 app.mount("/wordle", StaticFiles(directory="static", html=True), name="static")
 
